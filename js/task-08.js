0a9f195c9eb422a5fd82b33f19bcc1bd45b436f5
NataliaStupka <NataliGluschenko@ukr.net>
2021-10-24 12:13:36 +0300

    предупреждение перенесено в alert

--- js/task-08.js
+++ js/task-08.js
@@ -2,20 +2,33 @@
 const formEl = document.querySelector(".login-form");
 formEl.addEventListener("submit", onFormSubmit);
 
+// function onFormSubmit(event) {
+//     event.preventDefault();
+
+//     const { elements: { email, password }} = event.currentTarget;
+//     if (email.value === "" || password.value === "") {
+        
+//     return console.log("Все поля должны быть заполненны!");
+//   }
+
+//     console.log(`Email: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
+
+
+
 function onFormSubmit(event) {
     event.preventDefault();
 
     const { elements: { email, password }} = event.currentTarget;
     if (email.value === "" || password.value === "") {
         
-    return console.log("Все поля должны быть заполненны!");
+    return alert("Все поля должны быть заполненны!");
   }
 
     console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
-
-
 
 
 
